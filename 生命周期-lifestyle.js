475565493396ad451ce1c6df5be36d8671695cec
hejianxinsir <50935669+hejianxinsir@users.noreply.github.com>
2019-11-27 11:15:43 +0800

    Create 生命周期-lifestyle.js

---
+++ 生命周期-lifestyle.js
@@ -0,0 +1,31 @@
+// 生命周期四个过程 : 生 挂 更 死
+
+let app = document.getElementById('app');
+
+// create div  生：创建元素
+let div = document.createElement('div');
+div.style.border = '1px solid red';
+
+let state = 0;
+div.innerHTML = `
+  <p>${state}</p>
+  <button>+1</button>
+  <button>die</button>
+`
+
+// mount div 挂：挂载元素
+app.appendChild(div);
+
+div.querySelector('button').onclick = ()=>{
+  state += 1
+  
+  // update div
+  div.querySelector('p').innerText = state;
+};
+
+div.querySelectorAll('button')[1].onclick = ()=>{
+  div.querySelector('button').onclick = nunll;
+  div.querySelector('button')[1].onclick = null;
+  div.remove();
+  div = null;   // destroy div
+};
