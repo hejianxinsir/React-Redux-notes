29254aa67760e60d8c97832f12b5dfb9cd28d190
hejianxinsir <50935669+hejianxinsir@users.noreply.github.com>
2019-12-15 23:16:02 +0800

    Create window-history.pushState.js

---
+++ window-history.pushState.js
@@ -0,0 +1,48 @@
+
+// window.history.pushState(null,'','/login')
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {useState} from 'react';
+
+import "./styles.css";
+
+function Box1(){
+  return(
+    <div className="box1">注册</div>
+  )
+}
+
+function Box2(){
+  return(
+    <div className="box2">登录</div>
+  )
+}
+
+function App() {
+  let hash = window.location.hash
+  let initUi = hash === '#signup' ? '注册' : '登录'
+  const [Ui, setUi] = useState(initUi);
+  console.log(initUi)
+  const onClickLogin = ()=>{
+    setUi('登录')
+    window.history.pushState(null, '', '/login')
+  }
+  const onClickSignup = ()=>{
+    setUi('注册')
+    window.history.pushState(null, '', '/signup')
+  }
+
+  return (
+    <div className="App">
+      <button onClick={onClickSignup}>注册</button>
+      <button onClick={onClickLogin}>登录</button>
+      <div className="boxWrap">
+        {Ui === '登录' ? <Box2 /> : <Box1/>}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
